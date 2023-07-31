66a9ae33bd36cab807162640c696a157ef3c8abd
President Raindas <presidentraindas@gmail.com>
2023-07-31 03:15:58 +0100

    Home View coin row view addition

---
+++ CryptoTracker/Core/Home/ViewModels/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  CryptoTracker
+//
+//  Created by President Raindas on 31/07/2023.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    @Published var allCoins: [CoinModel] = []
+    @Published var portfolioCoins: [CoinModel] = []
+    
+    init() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
+        }
+    }
+}

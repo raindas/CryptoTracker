8486e796d3ef2cab83ade1de5bc639e8e6694d38
President Raindas <presidentraindas@gmail.com>
2023-08-05 23:58:59 +0100

    image caching

--- CryptoTracker/Services/CoinImageService.swift
+++ CryptoTracker/Services/CoinImageService.swift
@@ -21,6 +21,7 @@ class CoinImageService {
     }
     
     private func getCoinImage() {
+        print("Downloading image now")
         guard let url = URL(string: coin.image) else { return }
         
         // download using combine
